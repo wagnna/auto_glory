de6ab1b49d4cba016353cb591ba538d4333e0cf2
wangna199305 <1415711435@qq.com>
2019-12-26 16:37:05 +0800

    first

--- configs/sendemail.py
+++ configs/sendemail.py
@@ -13,7 +13,11 @@ REPORT_PATH = os.path.join(os.path.split(os.path.dirname(__file__))[0],'reports.
 def send_mail(file_new):
     mail_from='1415711435@qq.com'#邮件发送地址
     #mail_to='xupeiqing@artbloger.com'#邮件接收地址
-    mail_to='wangna@hupu.com,1415711435@qq.com,linke@hupu.com,wangmengdi@hupu.com,liuyanyun@hupu.com,'#邮件接收地址
+    # mail_to='wangna@hupu.com,1415711435@qq.com,linke@hupu.com,wangmengdi@hupu.com,liuyanyun@hupu.com'#邮件接收地址
+    mail_to_user =['1415711435@qq.com','wangna@hupu.com','linke@hupu.com','wangmengdi@hupu.com','liuyanyun@hupu.com','dongshuming@hupu.com']
+    mail_to = ','.join(mail_to_user)
+    print(mail_to)
+    # [x.encode('utf-8') for x in mail_to]
     f = open(file_new,'rb')
     mail_body = f.read()
     f.close()
@@ -30,7 +34,7 @@ def send_mail(file_new):
     smtp.login(mail_from,'hewsirhbmsqticci')
                # 'rpdhayxdwuqxighi')
                #                # 'hewsirhbmsqticci')
-    smtp.sendmail(mail_from,mail_to,msg.as_string())
+    smtp.sendmail(mail_from,mail_to.split(","),msg.as_string())
     smtp.quit()
     print("发送邮件成功")
 
